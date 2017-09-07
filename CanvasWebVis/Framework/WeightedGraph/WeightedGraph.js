caa45ef2ddfc60126acdc5f5868776a66ecdfab2
Arne Osterthun <arne.osterthun@uni-weimar.de>
2017-09-07 14:02:19 +0200

    Crazy depth search shit

--- CanvasWebVis/Framework/WeightedGraph/WeightedGraph.js
+++ CanvasWebVis/Framework/WeightedGraph/WeightedGraph.js
@@ -9,6 +9,18 @@ function WeightedGraph(_json_graph){
 	this.vertex_count_at_depth = _json_graph.vertex_count_at_depth;
 	this.max_vertex_title_size_at_depth = [];
 	this.vertex_rect_padding = 0;
+
+	this.vertecies.forEach(function(_vertex){
+		_vertex.highlight_state = false;
+	});
+
+	this.vertecies.forEach(function(_vertex){
+		_vertex.color = "#C1C1C1";
+	});
+
+	this.edges.forEach(function(_edge){
+		_edge.color = "#C1C1C1";
+	});
 }
 
 WeightedGraph.prototype.containsVertexAtCoords = function(_coords)
@@ -18,7 +30,6 @@ WeightedGraph.prototype.containsVertexAtCoords = function(_coords)
 				(((_vertex.coords.y_abs + (_vertex.font_size / 2)) >= _coords.y) && (_vertex.coords.y_abs) <= (_coords.y+ (_vertex.font_size / 2)));
 	});
 
-	console.log(_vertex_at_coords);
 
 	// _vertex_at_coords = this.vertecies.filter(function(_vertex){
 	// 	return (Math.pow((_coords.x - _vertex.coords.x_abs),2) + Math.pow((_coords.y - _vertex.coords.y_abs),2)) < Math.pow(_vertex.size,2);
@@ -33,10 +44,13 @@ WeightedGraph.prototype.getAdjacentVertecies = function(_vertex)
 	_adjacent_vertecies = [];
 
 	this.edges.forEach(function(_edge){
-		if ((_vertex.id == _edge.source_vertex_id) || (_vertex.id == _edge.dest_vertex_id))
+		if (_vertex.id == _edge.source_vertex_id)
+		{
+			_adjacent_vertecies.push(this.getVertexById(_edge.dest_vertex_id));
+		}
+		else if(_vertex.id == _edge.dest_vertex_id)
 		{
 			_adjacent_vertecies.push(this.getVertexById(_edge.source_vertex_id));
-			_adjacent_vertecies.push(this.getVertexById(_edge.dest_vertex_id));
 		}
 	},this);
 	
@@ -79,19 +93,70 @@ WeightedGraph.prototype.getIncommingEdges = function(_vertex)
 	return _edges;
 }
 
-WeightedGraph.prototype.depth_first_search = function(_start_vertex, _visited, _callback)
+WeightedGraph.prototype.depth_search = function(_start_vertex,_depth)
 {
-	_visited.push(_start_vertex);
-	_callback(_start_vertex);
-	this.getAdjacentVertecies(_start_vertex).forEach(function(_adjacent_vertex){
-		if(!~_visited.indexOf(_adjacent_vertex))
-		{
-			if(_adjacent_vertex.coords.x >= _start_vertex.coords.x)
+	_current_depth = 0;
+	_visited = [];
+
+	_current = [_start_vertex.id];
+	while(_current_depth < _depth){
+		console.log("depth: " + _current_depth);
+		_next = [];
+		_current.forEach(function(_id){
+			if(!~_visited.indexOf(_id))
 			{
-				this.depth_first_search(_adjacent_vertex,_visited,_callback);
+				_visited.push(_id);
+				_adjacent_vertices = this.getAdjacentVertecies(this.getVertexById(_id));
+				console.log("Channel: " + this.getVertexById(_id).channel_data.title);
+				console.log("Adj Count: " + _adjacent_vertices.length);
+				_next = _next.concat(_adjacent_vertices.map(function(_adjacent_vertex){return _adjacent_vertex.id})
+											.filter(function(_id){ return (!~_visited.indexOf(_id))}));
+
+				console.log("Next post filtered: " + _next.length);
+				//_visited = _visited.concat(_next);
+				//console.log(_visited);
 			}
-		}
-	},this);	
+		},this);
+		_current = _next;
+		_current_depth++;
+		//console.log("depth: " + _current_depth);	
+	}
+	return _visited;
+}
+
+WeightedGraph.prototype.depth_first_search = function(_start_vertex, _visited,_recursions, _callback)
+{
+	if(_recursions >= 0)
+	{
+		console.log("start: " + _start_vertex.channel_data.title);
+		_current = [];
+		_visited.push(_start_vertex);
+		_callback(_start_vertex);
+		this.getAdjacentVertecies(_start_vertex).forEach(function(_adjacent_vertex){
+			if(!~_visited.indexOf(_adjacent_vertex))
+			{
+				if(_adjacent_vertex.coords.x >= _start_vertex.coords.x)
+				{
+					console.log("adj: " + _adjacent_vertex.channel_data.title)
+					_current.push(_adjacent_vertex);
+					_visited.push(_adjacent_vertex);
+				}
+			}
+		},this);
+		
+		
+		_current.forEach(function(_vertex){
+			if(_start_vertex.coords.x != _vertex.coords.x)
+			{
+				this.depth_first_search(_vertex,_visited,_recursions--,_callback);	
+			}
+			else
+			{
+				this.depth_first_search(_vertex,_visited,_recursions,_callback);	
+			}
+		},this);	
+		
+	}
 }
 
 WeightedGraph.prototype.sortByNumberOfIncomingEdges = function()
@@ -112,9 +177,9 @@ WeightedGraph.prototype.sortByNumberOfIncomingEdges = function()
 	// 	}
 	// });
 
-	console.log(_edgeIncomeCount);
-	Object.keys(_edgeIncomeCount).sort();
-	console.log(_edgeIncomeCount);
+	//console.log(_edgeIncomeCount);
+	//Object.keys(_edgeIncomeCount).sort();
+	//console.log(_edgeIncomeCount);
 	//_edgeIncomeCount.sort();
 	//console.log(_edgeIncomeCount);
 }
@@ -208,7 +273,7 @@ WeightedGraph.prototype.drawVertex = function(_vertex)
 {
 	//this.draw_settings.context.arc(_vertex.coords.x,_vertex.coords.y,_vertex.size,0,2*Math.PI);
 	_font_size = (window.innerHeight) / (this.vertex_count_at_depth[_vertex.coords.x] + 1)
-	console.log(_vertex.coords.x);
+	//console.log(_vertex.coords.x);
 	if(_font_size > 16)
 	{
 		_font_size = 16;
@@ -343,7 +408,7 @@ WeightedGraph.prototype.calcVertexCoords = function()
 	this.vertecies.forEach(function(_vertex){
 
 		_depth_vertex_count = this.vertex_count_at_depth[_vertex.coords.x];
-		console.log("afd" + ((_vertex.font_size /1000) * 583));
+		//console.log("afd" + ((_vertex.font_size /1000) * 583));
 		_init_x_offset = ((window.innerWidth - this.max_vertex_title_size_at_depth[this.depth] * ((_font_size /1000) * 583))) / (this.depth*2) ;
 		_vertex.coords.x_abs = (_vertex.coords.x * (_init_x_offset * 2));
 		_init_y_offset = ((window.innerHeight) / (_depth_vertex_count *2) );
@@ -352,12 +417,12 @@ WeightedGraph.prototype.calcVertexCoords = function()
 		_vertex.size = 10;
 	},this);
 
-	console.log(this.max_vertex_title_size_at_depth)
+	//console.log(this.max_vertex_title_size_at_depth)
 }
 
 WeightedGraph.prototype.draw = function()
 {
-	console.log("[start]WeightedGraph.prototype.draw");
+	//console.log("[start]WeightedGraph.prototype.draw");
 
 	this.vertecies.forEach(function(_vertex){
 		this.drawVertex(_vertex);
@@ -380,5 +445,5 @@ WeightedGraph.prototype.draw = function()
 			this.drawEdge(_edge);
 		}
 	},this);
-	console.log("[end]WeightedGraph.prototype.draw");
+	//console.log("[end]WeightedGraph.prototype.draw");
 }
